cd96cf117fb3b47f69f682aab8cba0d492446bb9
Dawson M <dawson.mortenson@gmail.com>
2018-05-08 22:39:48 -0700

    add a basic otp worker

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,3 +1,14 @@
 defmodule MetexGenserver.Worker do
   use GenServer
+
+  # Client
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  #Server
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
 end
